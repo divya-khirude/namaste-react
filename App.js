de6f0bc45b77f5cd2e5dd764954494d3a7d35163
Divya Khirude <dkhirude@gmail.com>
2024-08-30 14:53:33 +0530

    Episode-03: Part 1

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is Namaste React 🚀"),
-    React.createElement("h2", {}, "by Divya Khirude"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+// React.createElement => Object => HTMLElement(render)
 
-console.log(parent); // object
+const heading = React.createElement('h1', { id: 'heading' }, 'Namaste React 🚀');
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);
 
-root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
