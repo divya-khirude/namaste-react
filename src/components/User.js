b0d448ed870bfd0b9b4fe55d12b73d29760aa355
Divya Khirude <dkhirude@gmail.com>
2024-09-10 09:05:09 +0530

    Episode-08: Part 1

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = ({ name }) => {
+  return (
+    <div className="user-card">
+      <h2>Name: {name}</h2>
+      <h3>Location: Nashik</h3>
+      <h4>Contact: @divya_khirude</h4>
+    </div>
+  );
+};
+
+export default User;
