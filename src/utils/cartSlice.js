3be42d5c2066d83b189ecbc17bfbaeb2e3f22cc2
Divya Khirude <dkhirude@gmail.com>
2024-09-14 22:08:59 +0530

    Episode-12: Part 2 (RTK mutating state and redux dev tools)

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -7,14 +7,20 @@ const cartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      // Mutating the state here
+      // Mutating the state here.
+      // Redux Toolkit uses Immer BTS
       state.items.push(action.payload);
     },
     removeItem: (state) => {
       state.items.pop();
     },
     clearCart: (state) => {
+      // RTK - Either mutate the existing state or return a new state
       state.items.length = 0;
+      // state.items = [];
+      // return {
+      //   items: [],
+      // };
     },
   },
 });
